c0f13c86f33bf2d46ce9540d5adc93b88f15aabc
Facundo Pietrobon <facundopietrobon@hotmail.com>
2023-02-21 23:24:21 -0300

    21-02

---
+++ src/public/js/main.js
@@ -0,0 +1,58 @@
+const socket = io();
+
+let user;
+const chatbox = document.querySelector("#chatbox");
+
+//sweetalers
+Swal.fire({
+    title: "Identificate",
+    input: "text",
+    text: "Ingresa tu nombre para identificarte en el chat",
+    //valida que el usuario este registrado para enviar mensaje
+    inputValidator: (value) => {
+      return !value && "Debes ingresar un nombre!";
+    },
+    //evita que con un click porfuera me deje entrar al chat
+    allowOutsideClick: false,
+    //hago que aparezca el usuario que envia el mensaje
+  }).then((res) => {
+    user = res.value;
+    // console.log(res.value);
+    socket.emit("inicio", "Inicio de sesion");
+  });
+  
+
+  //logica de envio con enter
+  chatbox.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      if (chatbox.value.trim().length > 0) {
+        console.log(chatbox.value);
+        socket.emit("message", { message: chatbox.value, user });
+        chatbox.value = "";
+      }
+    }
+  });
+  
+
+  //sweetalert de nuevo cliente conectado
+  socket.on("connected", (data) => {
+    if (user !== undefined) {
+      Swal.fire({
+        text: "Nuevo cliente conectado",
+        toast: true,
+        position: "top-right",
+      });
+    }
+  });
+  
+  socket.on("messageLogs", (data) => {
+    let log = document.querySelector("#messageLogs");
+    let messages = "";
+  
+    data.forEach((message) => {
+      messages =
+        messages + `<strong>${message.user}</strong>: ${message.message} <br>`;
+    });
+  
+    log.innerHTML = messages;
+  });
